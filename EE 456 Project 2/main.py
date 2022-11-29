8c14bbc62f18baa911b85f575ae996dc27fbeb12
Sameer Khan <sameerkhan@outlook.com>
2022-11-29 14:41:36 -0500

    added model

--- EE 456 Project 2/main.py
+++ EE 456 Project 2/main.py
@@ -1,3 +1,15 @@
 import tensorflow as tf
 
-(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data().load_data()
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), padding='same', activation="relu",input_shape=(32, 32, 3)),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=2),
+
+    tf.keras.layers.Conv2D(64, (3,3), padding='same', activation="relu"),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=2),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(100, activation="relu"),
+    tf.keras.layers.Dense(10, activation="softmax")
+])
