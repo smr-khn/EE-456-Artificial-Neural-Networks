10c827f1be27d07f409a942e43cb0f71a7c6362b
Sameer Khan <sameerkhan@outlook.com>
2022-11-29 14:51:50 -0500

    updated model

--- EE 456 Project 2/main.py
+++ EE 456 Project 2/main.py
@@ -6,10 +6,15 @@ model = tf.keras.Sequential([
     tf.keras.layers.Conv2D(32, (3,3), padding='same', activation="relu",input_shape=(32, 32, 3)),
     tf.keras.layers.MaxPooling2D((2, 2), strides=2),
 
-    tf.keras.layers.Conv2D(64, (3,3), padding='same', activation="relu"),
+    tf.keras.layers.Conv2D(32, (3,3), padding='same', activation="relu"),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=2),
+
+    tf.keras.layers.Conv2D(32, (3,3), padding='same', activation="relu"),
     tf.keras.layers.MaxPooling2D((2, 2), strides=2),
 
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(100, activation="relu"),
     tf.keras.layers.Dense(10, activation="softmax")
 ])
+
+model.summary()
