e4ebc127684f772f5dfbc2d2b93aca88ffe53115
Allen Reed <amr7271@psu.edu>
2022-11-29 15:24:27 -0500

    Trying to get graphs to work

--- EE 456 Project 2/main.py
+++ EE 456 Project 2/main.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.Sequential([
     tf.keras.layers.Conv2D(32, (3,3), padding='same', activation="relu",input_shape=(32, 32, 3)),
@@ -18,3 +19,23 @@ model = tf.keras.Sequential([
 ])
 
 model.summary()
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+from tensorflow.keras.callbacks import EarlyStopping
+callbacks = [
+             EarlyStopping(patience=2)
+]
+
+history = model.fit(x_train,y_train, epochs=1,validation_data=(x_test,y_test),callbacks=callbacks)
+
+import pandas as pd
+metrics_df = pd.DataFrame(history.history)
+
+metrics_df[["accuracy","val_accuracy"]].plot();
+
+model.save("model.h5")
+
+load_saved_model = tf.keras.models.load_model("model.h5")
